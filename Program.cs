679eac72e5b65e92bfe15db240297632910fc444
movinggun0219 <daugun@naver.com>
2025-07-07 15:09:38 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿internal class Program
+{
+    /// <summary>
+    /// Main 함수 
+    /// </summary>
+    /// <param name="args"></param>
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"{Solution07072(3, 4)}");
+    }
+
+    /// <summary>
+    ///  두 수의 곱하기 연산
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public static int Solution07072(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+    /// <summary>
+    /// 빼기 연산
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public static int Solution0707(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
+    }
+    
+    /// <summary>
+    ///  나머지 구하기
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public static int Solution0704(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 % num2;
+        return answer;
+    }
+}
